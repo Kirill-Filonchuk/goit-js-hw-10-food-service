3e3e2966362675ab60c9a8b3eb36fed3fa84e9d8
Kirill Filonchuk <kir.realtor@gmail.com>
2021-06-13 18:36:25 +0300

    third step

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './sass/main.scss';
+import menuCards from './menu.json';
+console.log(menuCards);
+
+import cardMenuTpl from './templates/cards.hbs';
+console.log(cardMenuTpl);
+
+const container = document.querySelector('.js-menu');
+console.log(container);
+//  список тем 
+// const Theme = {
+//     LIGHT: 'light-theme',
+//     DARK: 'dark-theme',
+//   };
+
+function createMenuCards (menuCards) {
+    return cardMenuTpl(menuCards);
+}
+const markupMenuCards = createMenuCards(menuCards);
+console.dir(markupMenuCards);
+
+container.innerHTML=markupMenuCards;
