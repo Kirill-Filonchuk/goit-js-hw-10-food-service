959658d1cd66e542a8fa76bde6b7af3410826310
Kirill Filonchuk <kir.realtor@gmail.com>
2021-06-14 13:29:16 +0300

    release
    on the way to the finish line

--- src/index.js
+++ src/index.js
@@ -1,22 +1,65 @@
 import './sass/main.scss';
 import menuCards from './menu.json';
-console.log(menuCards);
 
 import cardMenuTpl from './templates/cards.hbs';
-console.log(cardMenuTpl);
+
 
 const container = document.querySelector('.js-menu');
-console.log(container);
-//  список тем 
-// const Theme = {
-//     LIGHT: 'light-theme',
-//     DARK: 'dark-theme',
-//   };
-
 function createMenuCards (menuCards) {
     return cardMenuTpl(menuCards);
 }
 const markupMenuCards = createMenuCards(menuCards);
-console.dir(markupMenuCards);
 
 container.innerHTML=markupMenuCards;
+
+
+const Theme = {
+        LIGHT: 'light-theme',
+        DARK: 'dark-theme',
+      };
+
+const refs={
+    checkStatus : document.getElementById('theme-switch-toggle'),
+    mainPage : document.querySelector('body'),
+}
+
+console.log(refs.checkStatus);
+
+const onCheckTheme = (e) =>{ 
+   localStorage.setItem("status", '');
+
+if(e.target.checked){
+    localStorage.setItem("status", 'checked');
+    const readDataLS=localStorage.getItem("status");
+    console.log(readDataLS);  
+    getAttribute(readDataLS)
+
+} else {
+     removeAttribute()
+}
+    
+    console.log(e.target.checked)
+}
+
+localStorage.getItem("status") !== null && getAttribute(localStorage.getItem("status"))
+
+function  getAttribute(readDataLS) {
+    refs.checkStatus.setAttribute(readDataLS,'')
+
+    refs.mainPage.classList.remove(Theme.LIGHT);
+    refs.mainPage.classList.add(Theme.DARK);
+}
+
+function  removeAttribute() {
+    refs.checkStatus.removeAttribute('checked')
+    localStorage.removeItem("status")
+
+
+    refs.mainPage.classList.remove(Theme.DARK);
+    refs.mainPage.classList.add(Theme.LIGHT);
+}
+
+// слушатель события - статус check - box
+refs.checkStatus.addEventListener('change', onCheckTheme);
+
+
